945e81af4ad014a7becc2a8d43ec5f6d3a0afeca
Chris White <chris.white@rackspace.com>
2020-08-13 09:04:05 -0500

    Update examples
    * Fixed table encryption parameter
    * Update examples to show more practical values

--- examples/example.tf
+++ examples/example.tf
@@ -8,46 +8,46 @@ provider "aws" {
 }
 
 module "dynamo_table_provisioned" {
-  source = "git@github.com/rackspace-infrastructure-automation/aws-terraform-dynamo//?ref=v0.12.2"
+  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-dynamo//?ref=v0.12.2"
 
   enable_pay_per_request = false
-  hash_key               = "<HashKeyName>"
-  name                   = "<TableName>"
+  hash_key               = "user_id"
+  name                   = "corp_office"
   point_in_time_recovery = true
-  range_key              = "<RangeKey>"
+  range_key              = "department_id"
   read_capacity_units    = 5
-  table_encryption       = true
+  table_encryption_cmk   = true
   write_capacity_units   = 10
 
   attributes = [
     {
-      name = "<HashKeyName>"
+      name = "user_id"
       type = "S"
     },
     {
-      name = "<RangeKey>"
+      name = "department_id"
       type = "N"
     },
   ]
 }
 
 module "dynamo_table_pay_per_requst" {
-  source = "git@github.com/rackspace-infrastructure-automation/aws-terraform-dynamo//?ref=v0.12.2"
+  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-dynamo//?ref=v0.12.2"
 
   enable_pay_per_request = true
-  hash_key               = "<HashKeyName>"
-  name                   = "<TableName>"
+  hash_key               = "user_id"
+  name                   = "branch_office"
   point_in_time_recovery = true
-  range_key              = "<RangeKey>"
-  table_encryption       = true
+  range_key              = "department_id"
+  table_encryption_cmk   = true
 
   attributes = [
     {
-      name = "<HashKeyName>"
+      name = "user_id"
       type = "S"
     },
     {
-      name = "<RangeKey>"
+      name = "department_id"
       type = "N"
     },
   ]
